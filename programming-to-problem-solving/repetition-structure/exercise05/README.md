309072ae6a1f6252745a2bcd1564a00690e8842b
gabrielkist <gabrielkist02@gmail.com>
2023-09-18 20:23:13 -0300

    feat: exercise 05

---
+++ programming-to-problem-solving/repetition-structure/exercise05/README.md
@@ -0,0 +1,5 @@
+# Exercise 05
+
+Faça um algoritmo que escreve a tabuada até 10 dos dez primeiros números inteiros positivos permitindo a
+repetição do cálculo para novo número após ler “S” ou “N” como resposta para: Escrever nova tabuada
+(S/N)?.
---
+++ programming-to-problem-solving/repetition-structure/exercise05/exercise05.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <locale.h>
+#include <stdbool.h>
+
+#define NUMBER_TO_STOP_LOOP 0
+
+void printMultiplicationTable(int numberToCalculate)
+{
+  printf("Taboada do %i \n", numberToCalculate);
+  for (int i = 1; i <= 10; i++)
+  {
+    int multiplicationResult = numberToCalculate * i;
+    printf("%i x %i = %i \n", numberToCalculate, i, multiplicationResult);
+  }
+}
+
+void readUserInput(int *inputtedNumber)
+{
+  printf("Digite um número inteiro para calcular a taboada: \n");
+  scanf("%i", inputtedNumber);
+}
+void readIfHasToContinue(char *charVariable)
+{
+  printf("Caso deseje parar digite 'N', para continuar 'S' \n");
+  scanf("%s", charVariable);
+}
+
+bool isOddNumber(int number)
+{
+  return number % 2 == 0;
+}
+
+int main()
+{
+  int userInputNumber;
+  char continueMultiplicationTable;
+  setlocale(LC_ALL, "Portuguese");
+
+  do
+  {
+    readUserInput(&userInputNumber);
+    printMultiplicationTable(userInputNumber);
+    readIfHasToContinue(&continueMultiplicationTable);
+  } while (toupper(continueMultiplicationTable) == toupper('S'));
+}
